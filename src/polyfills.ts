f6a9da65216c6bf157afbf66eecdfe28be479172
SouravPatra <sourav.patra@soroco.com>
2020-10-04 18:52:02 +0530

    Functional suuport for IE

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -23,10 +23,19 @@
 
 /** IE9, IE10 and IE11 require all of the following polyfills. */
 import "core-js/es/array";
+import "core-js/es/date";
 import "core-js/es/function";
 import "core-js/es/map";
+import "core-js/es/math";
+import "core-js/es/number";
 import "core-js/es/object";
+import "core-js/es/parse-float";
+import "core-js/es/parse-int";
+import "core-js/es/regexp";
+import "core-js/es/set";
 import "core-js/es/string";
+import "core-js/es/symbol";
+import "core-js/es/weak-map";
 
 /**
  * Web Animations `@angular/platform-browser/animations`
