bc389be1bf0f7689bff45bff4654ba70b78c50c6
SouravPatra <sourav.patra@soroco.com>
2020-10-04 17:44:04 +0530

    Initial support for IE - not complete

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -21,6 +21,13 @@
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
+/** IE9, IE10 and IE11 require all of the following polyfills. */
+import "core-js/es/array";
+import "core-js/es/function";
+import "core-js/es/map";
+import "core-js/es/object";
+import "core-js/es/string";
+
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
@@ -56,6 +63,21 @@
  * Zone JS is required by default for Angular itself.
  */
 import "zone.js/dist/zone"; // Included with Angular CLI.
+
+/**
+ * By default, zone.js will patch all possible macroTask and DomEvents
+ * user can disable parts of macroTask/DomEvents patch by setting following flags
+ */
+
+(window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+(window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+(window as any).__zone_symbol__BLACK_LISTED_EVENTS = ["scroll", "mousemove"]; // disable patch specified eventNames
+
+/*
+ * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
+ * with the following flag, it will bypass `zone.js` patch for IE/Edge
+ */
+(window as any).__Zone_enable_cross_context_check = true;
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
